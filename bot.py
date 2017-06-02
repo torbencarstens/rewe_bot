5a1b23c82f9b02fd279c3e00fc1628be7c1d4f9f
Chabare <chabare95@gmail.com>
2017-06-02 12:22:35 +0200

    Add command

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import json
 from telegram.bot import Bot
 from telegram.ext import CommandHandler, Updater
 from rewe_offers import get
+from wanted import WantedProducts
 
 
 def get_token(filename: str = "secrets.json"):
@@ -15,20 +16,29 @@ def offers(bot: Bot, update):
         bold_price = "**{}**".format(price)
         return "[{}] {}".format(bold_price, name)
 
-    l = []
+    products = []
 
     market_id = "1487799323156"
     wanted_filename = "wanted.json"
     for name, price in get(market_id=market_id, wanted_filename=wanted_filename).items():
-        l.append("[{}] {}".format(price, name))
+        products.append(_get_product_printable(name, price))
 
-    bot.send_message(chat_id=update.message.chat_id, text="\n".join(l))
+    bot.send_message(chat_id=update.message.chat_id, text="\n".join(products))
+
+
+def list_wanted(bot: Bot, update):
+    wanted_filename = "wanted.json"
+    wanted_products = WantedProducts(wanted_filename).get_products()
+
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="\n".join([product.get_name() for product in wanted_products]))
 
 
 def run(token: str):
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("list", offers))
+    dispatcher.add_handler(CommandHandler("list_wanted", list_wanted))
 
     updater.start_polling()
 
