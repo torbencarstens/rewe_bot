10ed67b8d0187828a7b82984ff139ef9b7b615cd
Chabare <chabare95@gmail.com>
2017-06-02 11:55:26 +0200

    Extract classes from rewe_angebote

--- rewe_angebote.py
+++ rewe_angebote.py
@@ -1,92 +1,24 @@
-import requests
-from bs4 import BeautifulSoup, PageElement
-import re
-from typing import Dict, Generator, List, Union
+from offers import OffersWebsite
+from wanted import WantedProducts
+from typing import Dict, Set
 
 
-def get_market_content(url) -> Union[bytes, str]:
-    req = requests.get(url)
-    return req.content
+def get_acceptable_offers(*, market_id: str, wanted_filename: str) -> Set[Dict[str, float]]:
+    acceptable_offers_mappings = WantedProducts(wanted_filename).get_all_mappings()
+    offers = OffersWebsite(market_id).get_offers()
+    products = {}
+    for offer in offers:
+        for mapping in acceptable_offers_mappings:
+            if mapping.lower() in offer.get_name().lower():
+                products[offer.get_name()] = offer.get_price()
+
+    return products
 
 
-def get_soup(content) -> BeautifulSoup:
-    return BeautifulSoup(content, "html.parser")
-
-
-def get_products_outer(soup: BeautifulSoup) -> List[PageElement]:
-    return soup.find_all(class_="controller product")
-
-
-def get_product(product_outer) -> Dict[str, str]:
-    product_text_raw = product_outer.find(class_='dotdot').find('div').text
-    product_text_splitted = product_text_raw.split("\n")
-    product_text_oneline = " ".join(product_text_splitted)
-    return re.sub("\s+", " ", product_text_oneline)
-
-
-def get_picture_link(product_outer) -> str:
-    return product_outer.find('img')['href']
-
-
-def get_picture(link) -> Union[bytes, str]:
-    return requests.get(link).content
-
-
-def get_price(product_outer) -> float:
-    return float(re.findall(r"\d+.*", product_outer.find(class_='price').text)[0])
-
-
-def get_products(raw_products) -> Generator[Dict[str, str], None, None]:
-    for raw_product in raw_products:
-        yield {"price": get_price(raw_product), "name": get_product(raw_product)}
-
-
-def read_offers_json(filename="angebote.json"):
-    import codecs
-    import json
-
-    with codecs.open(filename, "r+", "utf-8") as angebote:
-        return json.load(angebote)
-
-
-def get_acceptable_offers_json(d) -> List[Dict[str, List[str]]]:
-    for product in d['products']:
-        for product_name, value in product.items():
-            product_mapping = value['mappings']
-            yield {'name': product_name, 'mappings': product_mapping}
-
-
-def get_acceptable_offers(products) -> Generator[Dict, None, None]:
-    acceptable_offers = read_offers_json()
-    acceptable_offers = list(get_acceptable_offers_json(acceptable_offers))
-    for product in products:
-        for acceptable_offer in acceptable_offers:
-            for alias in acceptable_offer['mappings']:
-                if alias.lower() in product['name'].lower():
-                    yield product
-
-
-def get():
-    base_url = "https://www.rewe.de/angebote/?marketChosen="
-    market_id = "1487799323156"
-    url = base_url + market_id
-    content = get_market_content(url)
-    soup = get_soup(content)
-    outer_products = get_products_outer(soup)
-
+def get(*, market_id, wanted_filename):
     # noinspection PyTypeChecker
-    products = list(get_products(outer_products))
-    products.sort(key=lambda product: product["price"])
-    # print(products)
-    # noinspection PyTypeChecker
-    acceptables = list(get_acceptable_offers(products))
-    products_dict = {}
+    return get_acceptable_offers(market_id=market_id, wanted_filename=wanted_filename)
 
-    for acceptable in acceptables:
-        if not products_dict.get(acceptable['name'], None):
-            products_dict[acceptable['name']] = acceptable['price']
-
-    return products_dict
 
 if __name__ == "__main__":
     # noinspection PyTypeChecker
