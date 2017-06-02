8cc1b6ec1cf65e502c6ad36c491e4b1a4dcbb977
Chabare <chabare95@gmail.com>
2017-06-02 12:11:15 +0200

    Add markdown Relates to #2

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import json
+from telegram.bot import Bot
 from telegram.ext import CommandHandler, Updater
-from rewe_angebote import get
+from rewe_offers import get
 
 
 def get_token(filename: str = "secrets.json"):
@@ -8,7 +9,12 @@ def get_token(filename: str = "secrets.json"):
         return json.load(secrets)['token']
 
 
-def offers(bot, update):
+def offers(bot: Bot, update):
+    # TODO: Add markdown
+    def _get_product_printable(name: str, price: float):
+        bold_price = "**{}**".format(price)
+        return "[{}] {}".format(bold_price, name)
+
     l = []
 
     market_id = "1487799323156"
@@ -16,7 +22,7 @@ def offers(bot, update):
     for name, price in get(market_id=market_id, wanted_filename=wanted_filename).items():
         l.append("[{}] {}".format(price, name))
 
-    bot.sendMessage(chat_id=update.message.chat_id, text="\n".join(l))
+    bot.send_message(chat_id=update.message.chat_id, text="\n".join(l))
 
 
 def run(token: str):
