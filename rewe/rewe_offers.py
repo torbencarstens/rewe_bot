4b534843804e279c46f858f7a625ece3e3df65b1
Chabare <chabare95@gmail.com>
2017-06-06 19:49:36 +0200

    Return offers from get

--- rewe/rewe_offers.py
+++ rewe/rewe_offers.py
@@ -7,11 +7,11 @@ from .wanted import WantedProducts
 def get_acceptable_offers(*, market_id: str, wanted_filename: str) -> Dict[str, float]:
     acceptable_offers_mappings = WantedProducts(wanted_filename).get_all_mappings()
     offers = OffersWebsite(market_id).get_offers()
-    products = {}
+    products = set()
     for offer in offers:
         for mapping in acceptable_offers_mappings:
             if mapping.lower() in offer.get_name().lower():
-                products[offer.get_name()] = offer.get_price()
+                products.add(offer)
 
     return products
 
