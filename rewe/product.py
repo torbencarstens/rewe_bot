035baec0e39c16de4920d46bdd7b188b7498283e
Chabare <chabare95@gmail.com>
2017-06-06 18:02:22 +0200

    Add classmethods

--- rewe/product.py
+++ rewe/product.py
@@ -9,6 +9,17 @@ class Product:
 
 
 class TelegramProduct(Product):
+    def __init__(self, *, name: str, picture_link: str = None, price: float = None):
+        super().__init__(name=name, picture_link=picture_link, price=price)
+
+    @classmethod
+    def from_offer(cls, offer):
+        name = offer.name
+        price = offer.price
+        picture_link = offer.picture_link
+
+        return cls(name=name, price=price, picture_link=picture_link)
+
     def get(self, *, markdown: bool = True, picture_link: bool = True):
         if picture_link and not markdown:
             raise AttributeError("Can't add link without markdown.")
