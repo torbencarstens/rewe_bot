9bfb6fc8cc0132f1cd11e2a785680383c5fc74c7
Chabare <chabare95@gmail.com>
2017-06-06 20:27:09 +0200

    Propagate log_level

--- rewe/rewe_offers.py
+++ rewe/rewe_offers.py
@@ -4,9 +4,12 @@ from .offers import OffersWebsite
 from .wanted import WantedProducts
 
 
-def get_acceptable_offers(*, market_id: str, wanted_filename: str) -> Dict[str, float]:
-    acceptable_offers_mappings = WantedProducts(wanted_filename).get_all_mappings()
-    offers = OffersWebsite(market_id).get_offers()
+def get_acceptable_offers(*, market_id: str, wanted_filename: str, log_level: str = "INFO") -> Dict[str, float]:
+    wanted_products = WantedProducts(wanted_filename, log_level=log_level)
+    offers_website = OffersWebsite(market_id, log_level=log_level)
+
+    acceptable_offers_mappings = wanted_products.get_all_mappings()
+    offers = offers_website.get_offers()
     products = set()
     for offer in offers:
         for mapping in acceptable_offers_mappings:
@@ -16,6 +19,6 @@ def get_acceptable_offers(*, market_id: str, wanted_filename: str) -> Dict[str, 
     return products
 
 
-def get(*, market_id, wanted_filename):
+def get(*, market_id, wanted_filename, log_level: str = "INFO"):
     # noinspection PyTypeChecker
-    return get_acceptable_offers(market_id=market_id, wanted_filename=wanted_filename)
+    return get_acceptable_offers(market_id=market_id, wanted_filename=wanted_filename, log_level=log_level)
