76f81f18f5b90c5605ce7b6252365698a27aea06
Chabare <chabare95@gmail.com>
2017-06-14 16:38:38 +0200

    Optionally return reason for finding offer

--- rewe/rewe_offers.py
+++ rewe/rewe_offers.py
@@ -1,11 +1,12 @@
-from typing import Dict
+from typing import Set, Tuple, Union
 
 from .logger import Logger
-from .offers import OffersWebsite
-from .wanted import WantedProducts
+from .offers import Offer, OffersWebsite
+from .wanted import WantedProduct, WantedProducts
 
 
-def get_acceptable_offers(*, market_id: str, wanted_filename: str, log_level: str = "INFO") -> Dict[str, float]:
+def get_acceptable_offers(*, market_id: str, wanted_filename: str, log_level: str = "INFO",
+                          return_reason: bool = False) -> Union[Set[Offer], Tuple[Set[Offer], WantedProduct]]:
     log = Logger("rewe_offers")
     log.debug("Get wanted")
     wanted_products = WantedProducts(wanted_filename, log_level=log_level)
@@ -18,17 +19,25 @@ def get_acceptable_offers(*, market_id: str, wanted_filename: str, log_level: st
     offers = offers_website.get_offers()
     products = set()
     log.debug("iterate")
+    reason = None
     for offer in offers:
-        for mapping in acceptable_offers_mappings:
-            if mapping.lower() in offer.get_name().lower():
-                products.add(offer)
-                break
+        for wanted in wanted_products.get_products():
+            for mapping in wanted.get_mappings():
+                if mapping.lower() in offer.get_name().lower():
+                    if return_reason:
+                        products.add((offer, wanted))
+                    else:
+                        products.add(offer)
+                    break
 
     log.debug("Found %d products.", len(products))
-    return products
+    result = products
+
+    return result
 
 
-def get(*, market_id, wanted_filename, log_level: str = "INFO"):
+def get(*, market_id, wanted_filename, log_level: str = "INFO", return_reason: bool = False):
     # noinspection PyTypeChecker
     Logger("rewe_offers", level=log_level).debug("get")
-    return get_acceptable_offers(market_id=market_id, wanted_filename=wanted_filename, log_level=log_level)
+    return get_acceptable_offers(market_id=market_id, wanted_filename=wanted_filename, log_level=log_level,
+                                 return_reason=return_reason)
