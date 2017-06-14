0c5b352d24da639b60e0d8cb9d2eebdc5a92abc4
Chabare <chabare95@gmail.com>
2017-06-14 17:30:01 +0200

    Use regex for matching
    #15

--- rewe/rewe_offers.py
+++ rewe/rewe_offers.py
@@ -1,3 +1,4 @@
+import re
 from typing import Set, Tuple, Union
 
 from .logger import Logger
@@ -14,21 +15,22 @@ def get_acceptable_offers(*, market_id: str, wanted_filename: str, log_level: st
     offers_website = OffersWebsite(market_id, log_level=log_level)
 
     log.debug("Get mappings")
-    acceptable_offers_mappings = wanted_products.get_all_mappings()
     log.debug("Get offers")
     offers = offers_website.get_offers()
     products = set()
     log.debug("iterate")
-    reason = None
     for offer in offers:
         for wanted in wanted_products.get_products():
             for mapping in wanted.get_mappings():
-                if mapping.lower() in offer.get_name().lower():
-                    if return_reason:
-                        products.add((offer, wanted))
-                    else:
-                        products.add(offer)
-                    break
+                try:
+                    if re.search(mapping.lower(), offer.get_name().lower()):
+                        if return_reason:
+                            products.add((offer, wanted))
+                        else:
+                            products.add(offer)
+                        break
+                except Exception as e:
+                    log.error("Regex error (%s): %s", mapping, e)
 
     log.debug("Found %d products.", len(products))
     result = products
